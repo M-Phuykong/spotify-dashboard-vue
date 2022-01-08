018d4911aa7bb6b9cb5e7e7c190f29da802c5643
Phuykong <53167700+M-Phuykong@users.noreply.github.com>
2022-01-08 16:08:01 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ Vue.config.productionTip = false
 
 //constant for now
 Vue.prototype.$home_uri = "http://localhost:8080/home";
-Vue.prototype.$client_id = "67d525ed562d48da86b39e4a10f48c6f";
-Vue.prototype.$client_secret = "b68020f8cd0642e9a6312bbfc1abe450"
+Vue.prototype.$client_id = "";
+Vue.prototype.$client_secret = "";
 
 
 new Vue({
